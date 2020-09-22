8a1d73d77636a3e134ced207a2e2aad4b85e36c5
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-09-22 22:32:05 +0200

    exercise 1

--- src/filterByTerm.spec.js
+++ src/filterByTerm.spec.js
@@ -1,11 +1,20 @@
 function filterByTerm(inputArr, searchTerm) {
-  return inputArr.filter(function (arrayElement) {
-    return arrayElement.url.match(searchTerm);
-  });
+  if (searchTerm) {
+    let res = [];
+    searchTerm = searchTerm.toLowerCase();
+    inputArr.forEach((element) => {
+      if (element.url.includes(searchTerm)) res.push(element);
+    });
+    return res;
+  }
+  return null;
+  // let res = inputArr.filter(function (arrayElement) {
+  //   return arrayElement.url.match(searchTerm);
+  // });
 }
 
 describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
+  it("should filter by a search term (link)", () => {
     const input = [
       { id: 1, url: "https://www.url1.dev" },
       { id: 2, url: "https://www.url2.dev" },
@@ -15,5 +24,8 @@ describe("Filter function", () => {
     const output = [{ id: 3, url: "https://www.link3.dev" }];
 
     expect(filterByTerm(input, "link")).toEqual(output);
+    expect(filterByTerm(input, "LINK")).toEqual(output); // New test
+    expect(filterByTerm(input, "")).toBeNull(); // New test
+    expect(filterByTerm(input, "uRI")).toEqual([]); // New test
   });
 });
